afb2eca279e3b6c9a5fc7f94efc20fbae287de95
ArashMidos <arshiamidos@gmail.com>
2019-11-17 01:12:51 +0330

    fini

--- gownload/main.go
+++ gownload/main.go
@@ -68,45 +68,57 @@ func (g *download) Init(n int) error {
 	return nil
 }
 func (g *download) Start(n int) {
-	g.Add(1)
-
 	fmt.Println("worder ",n)
 	c:=http.Client{}
 	req,_:=http.NewRequest("GET", g.addr, nil)
 	req.Header.Add("Range", g.ranges[n])
 	res,_:=c.Do(req)
+	//var ff *os.File
 	file,e:=os.OpenFile(fmt.Sprintf("%s/part%d",path.Join(os.Args[0],".."),n ), 
 		os.O_APPEND| os.O_CREATE| os.O_WRONLY | os.O_RDONLY, 0777)
+	// ff=file
 	if e!=nil{
+		fmt.Println(" CRAETE ")
 		panic(e.Error())
-	}
-	res.Body.Close()
+	} 
 	io.CopyN(file, res.Body, int64(g.size/g.n)+1) 
-	
+	res.Body.Close()
+	file.Close()
 	g.Done()
 }
 func (g *download) StartAll() {
 	for i := 0; i < len(g.ranges); i++ {
+		g.Add(1)
 		go g.Start(i)
+
+		// g.Start(i)
 	}
-	g.Wait()
+	g.Wait() 
+	fmt.Println("done printing ..")
 }
 
 func (g *download) ConcatParts(){
 	os.RemoveAll("./final.mp4")
-	for i := 0; i < 4; i++ {
-		file,e:=os.OpenFile(fmt.Sprintf("%s/part%d",path.Join(os.Args[0],".."),i ),
+	out,_:=os.OpenFile("./final.mp4", os.O_APPEND| os.O_CREATE| os.O_WRONLY | os.O_RDONLY , 0777)
+
+	for i := 0; i < int(g.n); i++ {
+		file,_:=os.OpenFile(fmt.Sprintf("%s/part%d",path.Join(os.Args[0],".."),i ),
 			 os.O_RDONLY, 0777)
-		if e !=nil{
-			panic(e.Error())
-		}
+		/* if e !=nil{
+			file,ee:=os.Create(fmt.Sprintf("%s/part%d",path.Join(os.Args[0],".."),i) )
+			if ee!=nil {
+				panic(ee.Error())
+			}
+			//panic(e.Error())
+		} */
 		f,_:=file.Stat()
 		var b =make([]byte, f.Size())
 		file.Read(b)
 		file.Close()
+		fmt.Println(len(b))
 		fmt.Println(b[len(b)-10:],f.Size())
-		out,_:=os.OpenFile("./final.mp4", os.O_APPEND| os.O_CREATE| os.O_WRONLY | os.O_RDONLY , 0777)
 		bb,e:=out.Write(b)
 		fmt.Println(bb,e)
 	}
+	out.Close()
 }
--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	d:=gownload.New("http://cdn9.git.ir/1398/02/Linkedin%20Designing%20a%20Book-git.ir/001-creating%20a%20book%20in%20indesign-git.ir.mp4")
 	d.Init(4)
 	fmt.Println(d)	
-	d.StartAll()//wait to finish 
+	d.StartAll()//wait to finish
 	d.ConcatParts()
 
 }
